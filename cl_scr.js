e914da3e35d74742a2bb890c9127780459bd57e8
Sneha Das <95439390+iamsnehadas@users.noreply.github.com>
2023-11-13 01:19:14 +0530

    Update cl_scr.js

--- cl_scr.js
+++ cl_scr.js
@@ -1,6 +1,7 @@
 function updateTime() {
   var today = new Date();
-  var date = today.toLocaleDateString();
+  var options = { day: '2-digit' , month: '2-digit' , year: 'numeric' };
+  var date = today.toLocaleDateString('en-GB', options);
   var time = today.toLocaleTimeString();
   document.getElementById("date").innerHTML = date;
   document.getElementById("time").innerHTML = time;
